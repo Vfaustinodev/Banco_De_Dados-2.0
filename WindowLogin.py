7f600d0416dffaf4892299422452d89c162fc31e
Bowie <f.v.nio.oliveira@hotmail.com>
2023-08-24 22:24:48 -0300

    Save

---
+++ WindowLogin.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# Criando a janela de login
+class LoginUser():
+
+        main_login_window = Tk()
+        main_login_window.title('Sistema de Cadastro de Usuários')
+        main_login_window.geometry('450x350')
+
+        def __init__(self):
+            self.main_login_window.mainloop()
+    
+
